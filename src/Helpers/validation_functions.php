0bf5057cf769345a092f561afc8db90e5a7e64f2
dfm18 <dfm18@users.noreply.github.com>
2025-01-25 21:09:48 -0500

    add getFieldErrors and isFieldInvalid functions

--- src/Helpers/validation_functions.php
+++ src/Helpers/validation_functions.php
@@ -7,6 +7,24 @@ function setValidationErrors($form, $errors)
     $_SESSION['validation_errors'][$form] = $errors;
 }
 
+function getFieldErrors($form, $field)
+{
+    if (isset($_SESSION['validation_errors']) && isset($_SESSION['validation_errors'][$form])
+        && isset($_SESSION['validation_errors'][$form][$field]))
+    {
+        return $_SESSION['validation_errors'][$form][$field];
+    }
+}
+
+function isFieldInvalid($form, $field): bool
+{
+    if (!isset($_SESSION['validation_errors'])) return false;
+
+    if (!isset($_SESSION['validation_errors'][$form])) return false;
+
+    return isset($_SESSION['validation_errors'][$form][$field]);
+}
+
 function clearValidationErrors()
 {
     if (isset($_SESSION['validation_errors'])) unset($_SESSION['validation_errors']);
